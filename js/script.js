713fd7e99a65da9f859255716c4131095b8da247
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-07-15 13:48:21 +0200

    set base Gameboard logic exposing methods: getBoard, placeMark, resetBoard

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Gameboard
+
+const Gameboard = (() => {
+	const rows = 3;
+	const columns = 3;
+	const board = [];
+
+	// Helper to set an empty board or reset it
+	function resetBoard() {
+		// Empty the board array. For truncating
+		// constants, which cannot be reassigned 
+		// like `board = [];`, we can set its length to
+		// zero to have it cleaned before rebuilding it.
+		board.length = 0;
+		for (let i = 0; i < rows; i++) {
+			// Add rows and columns in each row. Use an empty 
+			// string to make it easier to render the dashboard by 
+			// avoiding unnecessary checks. Use the format 
+			// 'Array(columns).fill("")' to avoid an inner loop.
+			board[i] = Array(columns).fill("");
+		}
+	};
+
+	// Call the function to reset the board
+	resetBoard();
+
+	// Return current dashboard
+	function getBoard() {
+		return board;
+	};
+
+	// Place a mark on the dashboard
+	function placeMark(row, column, mark) {
+		// Allow to mark just available spots
+		// identified by empty strings.
+		if (board[row][column] === "") {
+			board[row][column] = mark;
+		};
+	};
+
+	// Return the methods to be exposed
+	return { getBoard, placeMark, resetBoard };
+})();
